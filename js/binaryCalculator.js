b09606b42877f522103364609ad04a473992acdb
Eslam Amin <Eslam.amin.610@gmail.com>
2022-09-08 20:41:50 +0200

    Fixing Zero value

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -8,70 +8,74 @@
  */
 
 
-  
+
 var res = document.getElementById("res");
-function btnAddTextlick(obj){
+function btnAddTextlick(obj) {
     res.innerHTML += obj.innerHTML;
 }
-function btnClearClick(){
-    res.innerHTML='';
+function btnClearClick() {
+    res.innerHTML = '';
 }
 
-function btnEqulClick(){
+function btnEqulClick() {
     let re = /\d(\+|\-|\*|\/)\d/;
     let innerRes = res.innerHTML;
     let resultDecimalNum;
     let resultBinaryNum;
-    if(re.test(innerRes)){
+    if (re.test(innerRes)) {
         resultDecimalNum = convertToDecimal(innerRes);
         resultBinaryNum = converToBinary(resultDecimalNum);
         res.innerHTML = resultBinaryNum;
     }
-    else{
+    else {
         alert("This is invalid");
     }
 }
-function convertToDecimal(innerRes){
+function convertToDecimal(innerRes) {
     let splitRe = /[\+|\/|\*|\-]/;
     let arr = innerRes.split(splitRe);
-    let sign= innerRes.charAt(arr[0].length);
+    let sign = innerRes.charAt(arr[0].length);
     let firstDecimalNum = loopingThroughStr(arr[0]);
     let seconDecimalNum = loopingThroughStr(arr[1]);
 
     return getResult(firstDecimalNum, seconDecimalNum, sign);
 }
 
-function loopingThroughStr(str){
+function loopingThroughStr(str) {
     let decimalNum = 0;
-    for(let i=0,j=str.length-1;i<str.length;j--,i++){
-        decimalNum += parseInt(str[j]*(2**i));
+    for (let i = 0, j = str.length - 1; i < str.length; j--, i++) {
+        decimalNum += parseInt(str[j] * (2 ** i));
     }
     return decimalNum;
 }
-function getResult(firstNum, secondNum, sign){
-    switch(sign){
+function getResult(firstNum, secondNum, sign) {
+    switch (sign) {
         case '+':
-            return firstNum+secondNum;
+            return firstNum + secondNum;
         case '-':
-            return firstNum-secondNum;
+            return firstNum - secondNum;
         case '*':
-            return firstNum*secondNum;
+            return firstNum * secondNum;
         case '/':
-            if(secondNum!=0)
-                return firstNum/secondNum;
+            if (secondNum != 0)
+                return firstNum / secondNum;
     }
 
 }
 
-function converToBinary(decimalNum){
-    let binaryNum=["0"];
-    while(decimalNum > 0){
-        binaryNum=[];
-        if(decimalNum % 2 == 0)
-            binaryNum.unshift("0");
-        else
-        binaryNum.unshift("1");
-    decimalNum = parseInt(decimalNum / 2);
+function converToBinary(decimalNum) {
+    let binaryNum = [];
+    if (decimalNum == 0) {
+        return "0";
     }
-    return binaryNum.join("");
+    else {
+        while (decimalNum > 0) {
+            if (decimalNum % 2 == 0)
+                binaryNum.unshift("0");
+            else
+                binaryNum.unshift("1");
+            decimalNum = parseInt(decimalNum / 2);
+        }
+        return binaryNum.join("");
+    }
 }
